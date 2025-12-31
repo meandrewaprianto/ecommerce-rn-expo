3903d565e70676bd9b36211b8cbfdc769f0e22e2
meandrewaprianto <devandrew.aprianto@gmail.com>
2025-12-31 10:54:47 +0700

    small fix

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
