e710642a39b1d69589b4fd2fb60fc4752a79b66b
meandrewaprianto <devandrew.aprianto@gmail.com>
2026-01-03 15:27:10 +0700

    order routes & controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
 
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
@@ -20,6 +21,7 @@ app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
